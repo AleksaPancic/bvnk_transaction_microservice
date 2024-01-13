3f775ab23fb584befebd4b3864c3388ee668eb68
VPANCAL <aca.pancic@gmail.com>
2024-01-13 03:09:50 +0100

    Init

---
+++ src/main/java/com/example/bvnk_transaction_microservice/BvnkTransactionMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.example.bvnk_transaction_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BvnkTransactionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BvnkTransactionMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bvnk_transaction_microservice/BvnkTransactionMicroserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.bvnk_transaction_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class BvnkTransactionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
